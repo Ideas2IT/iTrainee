116ca792dc3639f4147ce889c0eff85ac4e013ba
Rajarajan <rramanathan@ntst.com>
2022-04-21 15:09:57 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace iTrainee.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
