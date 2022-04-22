e8d5a03566de6686a5c07c363c476b3c724a1e7b
KaviyaRamamoorthy <kaviya.ramamoorthy@ideas2it.com>
2022-04-22 14:17:44 +0530

    Added Topics model

---
+++ iTrainee.Models/Topics.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace iTrainee.Models
+{
+    public class Topics
+    {
+        public int Id { get; set; }
+
+        public int StreamId { get; set; }
+
+        public string Name { get; set; }
+
+        public string ReferenceURL { get; set; }
+
+    }
+}
