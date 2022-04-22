22c71ddf2fe5ca52bbc32008cf8ed291c846103b
Aarthi <aarthi.tamilselvan@ideas2it.com>
2022-04-22 14:16:21 +0530

    User and Base models added

---
+++ iTrainee.Models/Base.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace iTrainee.Models
+{
+    internal class Base
+    {
+        public Boolean IsActive { get; set; }
+
+        public string InsertedBy { get; set; }
+
+        public string InsertedOn { get; set; }
+
+        public string UpdatedBy { get; set; }
+
+        public string UpdatedOn { get; set; }
+
+    }
+}
