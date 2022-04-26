6bd591608f0c902b5d783b269f61ae0437479bb6
Aarthi <aarthi.tamilselvan@ideas2it.com>
2022-04-26 11:42:08 +0530

    just to commit and pull

--- iTrainee.MVC/Areas/Admin/Controllers/HomeController.cs
+++ iTrainee.MVC/Areas/Admin/Controllers/HomeController.cs
@@ -1,1 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Net.Http;
+using System.Text;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
+namespace iTrainee.MVC.Admin.Controllers
+{
+    public class TraineeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
