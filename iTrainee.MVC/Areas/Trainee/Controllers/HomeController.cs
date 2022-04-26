b2b5133f06feaf059ea569ca32a768752911b01e
Karthik <karthik.subramani@ideas2it.com>
2022-04-26 11:25:32 +0530

    area config

--- iTrainee.MVC/Areas/Trainee/Controllers/HomeController.cs
+++ iTrainee.MVC/Areas/Trainee/Controllers/HomeController.cs
@@ -1,1 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
 
+namespace iTrainee.MVC.Areas.Trainee.Controllers
+{
+    [Area("Trainee")]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
