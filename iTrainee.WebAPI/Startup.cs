f49ff72789d1b6a192b0a246318396808af67374
DESKTOP-G6JUJKT\poorani.bala <poorani.bala@ideas2it.com>
2022-04-28 07:56:46 +0530

    Added initial version of Service, Api, and repository methods for Stream class.

--- iTrainee.WebAPI/Startup.cs
+++ iTrainee.WebAPI/Startup.cs
@@ -1,4 +1,5 @@
 using iTrainee.Data;
+using iTrainee.Data.DataManager;
 using iTrainee.Data.Interfaces;
 using iTrainee.Services;
 using iTrainee.Services.Interfaces;
@@ -36,8 +37,13 @@ namespace iTrainee.WebAPI
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "iTrainee.WebAPI", Version = "v1" });
             });
 
-            services.AddSingleton<IUserRepository, UserRepository>();
+            //Service
             services.AddSingleton<IUserService, UserService>();
+            services.AddSingleton<IStreamService, StreamService>();
+
+            //Repositories
+            services.AddSingleton<IDataManager>(x => new DataManager(this.Configuration.GetConnectionString("TraineeDB")));
+            services.AddSingleton<IStreamRepository, StreamRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
