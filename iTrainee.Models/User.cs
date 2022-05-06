b409672cf60a775272e9f740cbb9a404008c5a4a
KaviyaRamamoorthy <kaviya.ramamoorthy@ideas2it.com>
2022-05-06 10:20:54 +0530

    Ajax for Trainee, mentor list, stored procedure changed for SaveUser.

--- iTrainee.Models/User.cs
+++ iTrainee.Models/User.cs
@@ -19,5 +19,11 @@ namespace iTrainee.Models
         public string UserName { get; set; }
 
         public string Password { get; set; }
+
+        public Boolean IsAdmin { get; set; }
+
+        public Boolean IsMentor { get; set; }
+
+        public Boolean IsTrainee { get; set; }
     }
 }
