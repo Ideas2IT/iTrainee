f7f799c16512c3b697c757425125ae9c27dcb323
Kaviya Ramamoorthy <Lenovo@DESKTOP-18I2L5A>
2022-06-01 15:57:34 +0530

    Added Stream in subtopics

--- iTrainee.MVC/Controllers/HomeController.cs
+++ iTrainee.MVC/Controllers/HomeController.cs
@@ -129,6 +129,13 @@ namespace iTrainee.Controllers
         {
             return View();
         }
+
+        [Microsoft.AspNetCore.Mvc.HttpPost]
+        public async Task<IActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync();
+            return RedirectToAction("Index", "Home");
+        }
     }
 }
 
